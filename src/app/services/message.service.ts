3a3ecd2c1580a49c6d607e310f5dc70f7887e956
Jakob Pupke <jakob.pupke@googlemail.com>
2016-11-18 01:13:26 +0100

    Pimp 43 receive previous msgs on join (#8)
    * receive previous msgs when user joins room
    
    * use real date generated by server
    
    * change oauth path accounting for changes in api code

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -68,12 +68,30 @@ export class MessageService {
     var that = this;
     this.stompClient.connect({}, (frame) => {
       for(let roomId of rooms) {
-        that.stompClient.subscribe('/rooms/message/' + roomId, function ( { body } ) {
-        let message: Message = JSON.parse(body);
-        // The timestamp should come from the server to prevent timezone issues
-        message.date = new Date();
-        that.messages[roomId].push(message);
-      });
+        
+        /** 
+         * This maps to the @SubscripeMapping in Spring and is only
+         * invoked once the user joins a room
+         */
+        that.stompClient.subscribe('/app/initial-messages/' + roomId, ( { body } ) => {
+          let initialMessages: Message[] = JSON.parse(body).map(
+            msg => this.dbEntityToMessage(msg)
+          )
+
+          // Prepend the initialMessages to the messages
+          // (There could already be new messages in it from the other subscription)
+          that.messages[roomId].unshift.apply(
+            that.messages[roomId], initialMessages
+          )
+          // Now we can actually unsubscribe... This will not be invoked again.
+          that.stompClient.unsubscribe('/app/initial-messages/' + roomId)
+        })
+
+        that.stompClient.subscribe('/rooms/message/' + roomId, ( { body } ) => {
+          let message: Message = this.dbEntityToMessage(JSON.parse(body))
+          that.messages[roomId].push(message)
+        })
+
     }
         
     }, (err) => {
@@ -81,11 +99,20 @@ export class MessageService {
     });
   }
 
-  getMessages() {
+  private dbEntityToMessage(dbMessage): Message {
+    const date = new Date(dbMessage.creationDate.epochSecond * 1000)
+    return {
+      message: String(dbMessage.message),
+      userName: String(dbMessage.userName),
+      date
+    }
+  }
+
+  getMessages(): MessageCollection {
     return this.messages;
   }
 
-  getCurrentRoom() {
+  getCurrentRoom(): string {
     return this.currentRoom;
   }
 
@@ -93,7 +120,7 @@ export class MessageService {
     this.currentRoom = room;
   }
 
-  getRooms() {
+  getRooms(): String[] {
     return this.rooms;
   }
 
