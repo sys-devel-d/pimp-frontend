1ebbc58006cedddd0c36a43a1acdc967e9dd3352
Jakob Pupke <jakob.pupke@gmail.com>
2016-11-22 21:27:39 +0100

    cleanup services in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { AuthService } from "./services/auth.service";
 import { MessageService } from "./services/message.service";
@@ -6,19 +6,20 @@ import { MessageService } from "./services/message.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [MessageService, AuthService]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PIMP';
-  router: Router;
-  authService: AuthService;
-  messageService: MessageService;
 
-  constructor(router: Router, authService: AuthService, messageService: MessageService) {
-    this.router = router;
-    this.authService = authService;
-    this.messageService = messageService;
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private messageService: MessageService) {}
+
+  ngOnInit() {
+    if(this.authService.isLoggedIn()) {
+      this.messageService.init();
+    }
   }
 
   logout() {
