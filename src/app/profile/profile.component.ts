fe697aef6a583b45a0f1b3cac2071ccad4bbdb6f
Kevin Goy <beasly@users.noreply.github.com>
2016-12-09 20:23:19 +0100

    Add user photo to profile component. PIMP-59

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from "../models/base";
-import { UserService } from "../services/user.service";
+import { User } from '../models/base';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -11,6 +11,8 @@ export class ProfileComponent implements OnInit {
   user: User = new User();
   userService: UserService;
   error: string;
+  photo: any;
+  photoFile: string;
 
   constructor(userService: UserService) {
     this.userService = userService;
@@ -24,7 +26,17 @@ export class ProfileComponent implements OnInit {
     this.userService
       .getProfileInformation()
       .subscribe(
-        result => this.user = result as User,
+        result => {
+          this.user = result as User;
+          if (this.user.photo) {
+            this.userService
+              .getUserPhoto(this.user.userName, this.user.photo)
+              .subscribe(
+                photo => this.photo = photo,
+                error => this.error = <any>error
+              );
+          }
+        },
         error => this.error = <any>error
       );
   }
