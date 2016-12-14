9b397668ff080e83418b08108ce8c3cf7c0dcff3
Jakob Pupke <jakob.pupke@gmail.com>
2016-12-14 23:47:25 +0100

    cleanup dom functions

--- src/app/commons/dom-functions.ts
+++ src/app/commons/dom-functions.ts
@@ -1,32 +1,22 @@
-declare var $:any;
+declare var $: any;
 
-export function shakeInput(selector:string) {
-    $(selector).addClass('invalid-params');
-    setTimeout( () => {
-        $(selector).removeClass('invalid-params');
-    }, 500);
-}
-
-export function showChatModal() {
-    $('#chat-modal').modal('show');
-}
-
-export function hideChatModal() {
-    $('#chat-modal').modal('hide');
-}
-
-export function showCalendarModal() {
-    $('#calendar-event-editor-modal').modal('show');
-}
-
-export function hideCalendarModal() {
-  $('#calendar-event-editor-modal').modal('hide');
+export function shakeInput(selector: string) {
+  $(selector).addClass('invalid-params');
+  setTimeout(() => {
+    $(selector).removeClass('invalid-params');
+  }, 500);
 }
 
 export function showAppModal() {
-    $('#app-modal').modal('show');
+  $('#app-modal').modal('show');
 }
 
 export function hideAppModal() {
-    $('#app-modal').modal('hide');
+  $('#app-modal').modal('hide');
+}
+
+export function scrollDownChatMessageContainer() {
+  setTimeout(() => {
+    $("#chat-message-container").scrollTop($("#chat-message-container")[0].scrollHeight);
+  }, 100);
 }
