91cd84e7668504a62b5f11cbcd891853bb2d406f
Kevin Goy <beasly@users.noreply.github.com>
2017-01-15 17:00:33 +0100

    Add patch, post, delete to groups service. PIMP-116

--- src/app/services/groups.service.ts
+++ src/app/services/groups.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Globals } from '../commons/globals';
-import { Team, Project } from '../models/groups';
+import { Team, Project, Group } from '../models/groups';
 import { AuthService } from './auth.service';
 import { Observable, Subject } from 'rxjs';
 
@@ -54,6 +54,41 @@ export default class GroupsService {
     this.allGroupsRequest(successFunc);
   }
 
+  patchGroup(type: string, key: string, body) {
+    const headers = { headers: this.authService.getTokenHeader() };
+    return this.http
+      .patch(Globals.BACKEND + type + '/id/' + key , body, headers)
+      .map(res => res.json())
+      .subscribe(
+        group => this.subscribeToGroup(group, name, type),
+        err => console.log(err)
+      );
+  }
+
+  postNewGroup(type: string, id) {
+    const group = {
+      name: `New ${type}-${id}`,
+      userNames: []
+    };
+    const headers = { headers: this.authService.getTokenHeader() };
+    return this.http
+      .post(Globals.BACKEND + type , group, headers)
+      .subscribe(
+        () => this.addGroup(group, type),
+        err => console.log(err)
+      );
+  }
+
+  removeGroup(type: string, group: Group) {
+    const headers = { headers: this.authService.getTokenHeader() };
+    return this.http
+      .delete(Globals.BACKEND + type + '/id/' + group.key, headers)
+      .subscribe(
+        () => this.deleteGroup(group, type),
+        err => console.log(err)
+      );
+  }
+
   private allGroupsRequest(successFunc) {
     const headers = { headers: this.authService.getTokenHeader() };
     const projectReq = this.http.get(Globals.BACKEND + 'project', headers)
@@ -76,6 +111,53 @@ export default class GroupsService {
       successFunc,
       err => console.log(err)
     );
+  }
+
+  private subscribeToGroup(group, name, type) {
+    if (type === 'team') {
+      const teams = this.allTeams
+        .map(team => {
+          if (team.name === name) {
+            return group;
+          }
+          return team;
+        });
+      this.allTeams = teams;
+      this.allTeamsChange.next(teams);
+    } else {
+      const projects = this.allProjects
+        .map(project => {
+          if (project.name === name) {
+            return group;
+          }
+          return project;
+        });
+      console.log(projects);
+      this.allProjects = projects;
+      this.allProjectsChange.next(projects);
+    }
+  }
+
+  private addGroup(group, type) {
+    if (type === 'team') {
+      this.allTeams.push(group);
+      this.allTeamsChange.next(this.allTeams);
+    } else {
+      this.allProjects.push(group);
+      this.allProjectsChange.next(this.allProjects);
+    }
+  }
+
+  private deleteGroup(group, type) {
+    if (type === 'team') {
+      const teamToRemoveIndex = this.allTeams.indexOf(name);
+      this.allTeams.splice(teamToRemoveIndex, 1);
+      this.allTeamsChange.next(this.allTeams);
+    } else {
+      const projectToRemoveIndex = this.allProjects.indexOf(name);
+      this.allProjects.splice(projectToRemoveIndex, 1);
+      this.allProjectsChange.next(this.allProjects);
+    }
   }
 
   tearDown() {
