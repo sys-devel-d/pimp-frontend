75a6ca871161cb24f28a4237f815a119083d2bc9
Kevin Goy <beasly@users.noreply.github.com>
2017-01-15 17:01:22 +0100

    Add CRUD functions to admin view. PIMP-116

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
-import {Team, Project} from '../models/groups';
+import {Team, Project, Group} from '../models/groups';
+import {User} from '../models/base';
 import GroupsService from '../services/groups.service';
-import AdminModalComponent from './modal/admin-modal.component';
+import {UserService} from '../services/user.service';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -11,18 +12,26 @@ import { Subscription } from 'rxjs';
 })
 export class AdminComponent implements OnInit, OnDestroy {
 
-  @ViewChild(AdminModalComponent) adminModalComponent: AdminModalComponent;
-
   private teams: Team[];
   private projects: Project[];
   private groupsSubscriptions: Subscription[];
+  private groupToEdit: Group;
+  private typeToEdit: string;
 
-  constructor(private groupsService: GroupsService) {
+  private userAddingMode: boolean = false;
+  private term: string;
+  private users: User[];
+  private addUserCallback: Function;
+
+  constructor(
+    private groupsService: GroupsService,
+    private userService: UserService) {
     let teamsSub = this.groupsService
       .allTeamsChange.subscribe(teams => this.teams = teams);
     let projectsSub = this.groupsService
       .allProjectsChange.subscribe(projects => this.projects = projects);
     this.groupsSubscriptions = [teamsSub, projectsSub];
+    this.addUserCallback = this.addUserToGroup.bind(this);
   }
 
   ngOnInit() {
@@ -34,15 +43,61 @@ export class AdminComponent implements OnInit, OnDestroy {
     this.groupsSubscriptions.forEach(sub => sub.unsubscribe());
   }
 
-  editName(name) {
-    console.log(name);
+  editName(name, group, type) {
+    group.name = name;
+    this.groupsService.patchGroup(type, group.key, group);
   }
 
-  addGroup() {
-    console.log('add');
+  addGroup(type: string) {
+    // since we have no proper error logging
+    // we have to make sure that every name is unique
+    // TODO: proper error logging
+    const id = this.newGuid();
+    this.groupsService.postNewGroup(type, id);
   }
 
-  editGroup(name) {
-    console.log('edit');
+  private newGuid() {
+    return 'xxxx-yxxx'.replace(/[xy]/g, function(c) {
+      let r = Math.random()*8|0, v = c === 'x' ? r : (r&0x3|0x8);
+      return v.toString(8);
+    });
+  }
+
+  editGroup(groupToEdit, type) {
+    this.userAddingMode = true;
+    this.groupToEdit = groupToEdit;
+    this.typeToEdit = type;
+  }
+
+  addUserToGroup(user) {
+    this.groupToEdit.userNames.push(user.userName);
+    this.groupsService
+      .patchGroup(this.typeToEdit, this.groupToEdit.key, this.groupToEdit);
+    this.userAddingMode = false;
+  }
+
+  private searchForUser(term, userList) {
+    if (this.term.length >= 3) {
+      this.userService.searchAll(this.term)
+        .subscribe(
+          (users: User[]) => {
+            const withoutMember =
+              user => userList.indexOf(user.userName) === -1;
+            this.users = users.filter(withoutMember);
+          },
+          err => console.error(err)
+        );
+    }
+  }
+
+  removeUser(name: string, group: Group, type: string) {
+    const userToRemoveIndex = group.userNames.indexOf(name);
+    group.userNames.splice(userToRemoveIndex, 1);
+    this.groupsService
+      .patchGroup(type, group.key, group);
+  }
+
+  removeGroup(group, type) {
+    this.groupsService.removeGroup(type, group);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,20 @@ import { CalendarComponent } from './calendar/calendar.component';
 import { ChatComponent } from './chat/chat.component';
 import GroupChatEditorComponent from './chat/editor/group-chat-editor.component';
 import { LoginComponent } from './login/login.component';
-import { RouterModule } from "@angular/router";
+import { RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { OtherProfileComponent } from './profile/other-profile.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RegisterComponent } from './register/register.component';
-import { AuthGuard } from "./commons/auth.guard";
-import { AuthService } from "./services/auth.service";
-import { UserService } from "./services/user.service";
+import { AuthGuard } from './commons/auth.guard';
+import { AuthService } from './services/auth.service';
+import { UserService } from './services/user.service';
 import GroupsService from './services/groups.service';
 import CalendarService from './services/calendar.service';
-import { MessageService } from "./services/message.service";
-import { UserSearchComponent } from "./user-search/user-search.component";
-import { CalendarSearchComponent } from "./user-search/calendar-search.component";
+import { MessageService } from './services/message.service';
+import { UserSearchComponent } from './user-search/user-search.component';
+import { AllUserSearchComponent } from './user-search/all-user-search.component';
+import { CalendarSearchComponent } from './user-search/calendar-search.component';
 import { HighlightDirective } from './directives/highlight.directive';
 import { RoomNamePipe } from './pipes/room-name.pipe';
 import InlineEdit from './inline-edit/inline-edit.component';
@@ -61,7 +62,8 @@ import AdminModalComponent from './admin/modal/admin-modal.component';
     ReadOnlyEventModalComponent,
     UserSelectionComponent,
     AdminComponent,
-    AdminModalComponent
+    AdminModalComponent,
+    AllUserSearchComponent
   ],
   imports: [
     BrowserModule,
