2225c5d369642f88d3911bf1533cefd33643f1d0
Jakob Pupke <jakob.pupke@gmail.com>
2017-02-06 12:25:26 +0100

    ES6

--- prod-server.js
+++ prod-server.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('*', function(request, response) {
+app.get('*', (request, response) => {
   response.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
 });
